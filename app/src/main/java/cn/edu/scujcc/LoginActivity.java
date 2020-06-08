083cdd41201c801dd0d83b17c491ebf54491f2f3
Xiaojiaj <997181378@qq.com>
2020-06-08 21:47:55 +0800

    添加登录注册接口

--- app/src/main/java/cn/edu/scujcc/LoginActivity.java
+++ app/src/main/java/cn/edu/scujcc/LoginActivity.java
@@ -1,14 +1,4 @@
 package cn.edu.scujcc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class LoginActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+public class LoginActivity {
 }
